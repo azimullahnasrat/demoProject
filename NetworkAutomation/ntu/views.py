6e8c056f175edf841ddee2a4841ce7ec1170b8a1
azimullahnasrat <azimullahnasrat005@gmail.com>
2024-07-08 19:07:29 +0430

    load

--- NetworkAutomation/ntu/views.py
+++ NetworkAutomation/ntu/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 # Create your views here.
 from django.shortcuts import render
 from netmiko import ConnectHandler
@@ -12,17 +11,25 @@ def ntu(request):
 
     var={'name':"azimullah"}
     return render(request,'admin/index.html',context=var)
+    ################
 
 def add_switch(request):
     if request.method == 'POST':
-        form = CiscoSwitchForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('switch_list')
+        switch_form = CiscoSwitchForm(request.POST)
+        if switch_form.is_valid():
+            switch_form.save()
+            return redirect('switch_list')  # Replace with your desired URL
+        else:
+            # Handle the second form submission
+            username2 = request.POST.get('username2')
+            email2 = request.POST.get('email2')
+            password2 = request.POST.get('password2')
+            # Implement your logic to create a new user
     else:
-        form = CiscoSwitchForm()
-    return render(request, 'admin/form.html', {'form': form})
+        switch_form = CiscoSwitchForm()
 
+    return render(request, 'admin/form.html', {'form': switch_form})
+##############
 def switch_list(request):
     switches = CiscoSwitch.objects.all()
     return render(request, 'admin/list_sw.html', {'switches': switches})
